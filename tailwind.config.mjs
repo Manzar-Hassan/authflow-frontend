ac101b3165927bb12261083bcee54d86a7b1878c
Manzar-Hassan <manzar.h@biits.in>
2025-03-01 09:14:35 +0530

    removes tailwind css config

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,7 +4,6 @@ export default {
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/@manzar-hassan/authflow/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {
