18415d57d14a6ebba542dcf8692830fc063408cf
Manzar-Hassan <manzar.h@biits.in>
2025-03-03 17:58:46 +0530

    Feat: adds logout functionality in homepage Card component

--- app/page.js
+++ app/page.js
@@ -1,11 +1,18 @@
+'use client'
+
 import { Card } from "@manzar-hassan/authflow";
+import { useRouter } from "next/navigation";
 import React from "react";
 
 const page = () => {
+
+    const router = useRouter();
+
   return (
     <Card
       title="Authflow"
       description="Creating Authflow is Amazing"
+      onNavigate={(path) => router.push(path)}
     />
   )
 };
